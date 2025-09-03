d5e3391a2411690edabf8d5235aa39dd9ef142e0
Mudit Garg <muditgarg1211@gmail.com>
2025-09-03 17:36:52 +0530

    refactor: Loader added

--- src/pages/FixedDeposit.tsx
+++ src/pages/FixedDeposit.tsx
@@ -1,10 +1,11 @@
 import { DataTable } from '@/components/DataTable';
+import Loader from '@/components/Loader';
 import { useFilteredTableData } from '@/hooks/useFilteredTableData';
 
 const FixedDeposit = () => {
   const { filteredData, columns, loading } = useFilteredTableData("Fixed Deposit");
 
-  if (loading) return <div className="p-6">Loading...</div>;
+  if (loading) return <Loader />;
 
   return (
     <div className="p-6">
