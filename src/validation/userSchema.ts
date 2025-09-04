4fe67f652bab4272c48af1375c182303e9ea69d3
Kishore Kumar G C <kishorekumar.gc@hcltech.com>
2025-09-04 12:02:18 +0530

    removed unnecessary validation from password field

--- src/validation/userSchema.ts
+++ src/validation/userSchema.ts
@@ -11,12 +11,12 @@ export const userSchema = yup.object().shape({
   password: yup
     .string()
     .required("Password is required")
-    .min(8, "Password must be at least 8 characters")
-    // .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
-    .matches(/[0-9]/, "Password must contain at least one number")
-    .matches(
-      /[@$!%*?&_]/,
-      "Password must contain at least one special character"
-    ),
+    .min(8, "Password must be at least 8 characters"),
+  // .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+  // .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+  // .matches(/[0-9]/, "Password must contain at least one number")
+  // .matches(
+  //   /[@$!%*?&_]/,
+  //   "Password must contain at least one special character"
+  // ),
 });
